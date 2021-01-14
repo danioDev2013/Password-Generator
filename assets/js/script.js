214dc3ae673fdcca7005259c73e9362c9a0d2d67
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-01-14 12:34:21 -0800

    update css, added a background image, updated the card to be alittle more transparent and the color

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 //function for generate password in the function write password
 function generatePassword() {
 //array for uppercase letters, lowercase letters, numbers, and special characters
-var specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " " , ' " ', "\\" ];
+var specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ];
 var numbersPass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -107,7 +107,7 @@ var retVal = "";
         return;
     }
     //creating a charset with the characterschoice arrays
-    var charset = charactersChoicearr.slice("");
+    var charset = charactersChoicearr
 
     
     
